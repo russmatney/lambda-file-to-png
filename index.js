95afbb112a0475cb18aca472409a9d81b40887d1
Russell Matney <russell.matney@gmail.com>
2015-04-02 18:55:01 -0400

    first shot, gif/jpg to png

--- index.js
+++ index.js
@@ -1,25 +1,74 @@
 var Q = require('q');
 var execute = require('lambduh-execute');
+var transformS3Event = require('lambduh-transform-s3-event');
+var validate = require('lambduh-validate');
 
 var pathToRenamePngs = './bin/rename-pngs.sh';
 var pathToFileToPng = "./bin/file-to-png.sh";
 var pathToFilesToPngs = "./bin/files-to-pngs.sh";
 
 exports.handler = function(event, context) {
-  var start = new Date();
+  // gif or jpg has just been uploaded - is specifed in event
+  //
+  // Create timelapse func will use these 3 steps as well:
+  //   download all "pngs_for_timelapse_zip_[timestamp]" zips for the key
+  //   unpack them all
+  //   mv them all to shared folder (merge)
+  //
+  // convert gif/jpg into pngs
+  // zip 'merge' folder
+  // upload as new zip[timestamp]
+  // delete whatever "zip_[timestamp]" were downloaded
 
+  var start = new Date();
   var result = {};
-  execute(result, {
-    shell: 'echo "hi there"',
-    logOutput: true
-  }).then(function(result) {
-    context.done()
-  }).fail(function(err) {
-    if(err) {
-      context.done(err)
-    } else {
-      context.done(new Error("Unspecifed fail."))
-    }
-  });
+
+  transformS3Event(result, event)
+
+    .then(function(result) {
+      console.log('Validating S3 event.');
+      console.log(result);
+      return validate(result, {
+        "srcKey": {
+          endsWith: "\\.gif",
+          endsWithout: "_\\d+\\.gif",
+          startsWith: "events/"
+        }
+      });
+    })
+
+    .then(function(result) {
+      //mkdir /tmp/downloaded/, clear if it exists
+      return download(result, {
+        srcKey: result.srcKey,
+        srcBucket: result.srcBucket,
+        downloadFilepath: '/tmp/downloaded/' + path.basename(result.srcKey)
+      });
+    })
+
+    .then(function(result) {
+      //prep binary to be called (on lambda)
+      if(!result.downloadFilePath) {
+        throw new Error('result expected downloadFilePath');
+      }
+      return execute(result, {
+        bashScript: pathToFileToPng,
+        bashParams: [
+          result.downloadFilePath
+        ],
+        logOutput: true
+      });
+    })
+
+    .then(function(result) {
+      context.done()
+    }).fail(function(err) {
+      if(err) {
+        context.done(err)
+      } else {
+        context.done(new Error("Unspecifed fail."))
+      }
+    });
+
 }
 
